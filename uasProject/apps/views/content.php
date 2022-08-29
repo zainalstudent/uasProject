655df4c71ff5247b2bba65f98c56fc0054cbcffe
muhammadkhoironi <109646511+muhammadkhoironi@users.noreply.github.com>
2022-08-29 19:53:48 +0700

    update file content.php
    saya menambah coding di file content.php

---
+++ uasProject/apps/views/content.php
@@ -0,0 +1,173 @@
+ <?php 
+  <!-- Kelas -->
+    <div class="container-fluid">
+      <div class="container-fluid">
+        <h2 style="font-weight: bold; font-size: 34px">
+          Kelas Terbuka Berbasis Industri
+        </h2>
+        <div class="row">
+          <!-- Card Kelas -->
+          <div class="col-md-3">
+            <div class="card mb-3">
+              <img
+                src="assets/card1.png"
+                class="card-img-top"
+                alt="Kelas Javascript"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Fullstack Javascript Expert</h5>
+                <a
+                  href="#"
+                  class="btn btn-primary mx-auto"
+                  style="background-color: #ff7300; width: 270px; border: none"
+                  >Pelajari</a
+                >
+              </div>
+            </div>
+          </div>
+          <!-- End Card Kelas -->
+          <!-- Card Kelas -->
+          <div class="col-md-3">
+            <div class="card mb-3">
+              <img
+                src="assets/card2.png"
+                class="card-img-top"
+                alt="Kelas Javascript"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Fullstack Javascript Expert</h5>
+                <a
+                  href="#"
+                  class="btn btn-primary mx-auto"
+                  style="background-color: #ff7300; width: 270px; border: none"
+                  >Pelajari</a
+                >
+              </div>
+            </div>
+          </div>
+          <!-- End Card Kelas -->
+          <!-- Card Kelas -->
+          <div class="col-md-3">
+            <div class="card mb-3">
+              <img
+                src="assets/card3.png"
+                class="card-img-top"
+                alt="Kelas Javascript"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Fullstack Javascript Expert</h5>
+                <a
+                  href="#"
+                  class="btn btn-primary mx-auto"
+                  style="background-color: #ff7300; width: 270px; border: none"
+                  >Pelajari</a
+                >
+              </div>
+            </div>
+          </div>
+          <!-- End Card Kelas -->
+          <!-- Card Kelas -->
+          <div class="col-md-3">
+            <div class="card mb-3">
+              <img
+                src="assets/card4.png"
+                class="card-img-top"
+                alt="Kelas Javascript"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Fullstack Javascript Expert</h5>
+                <a
+                  href="#"
+                  class="btn btn-primary mx-auto"
+                  style="background-color: #ff7300; width: 270px; border: none"
+                  >Pelajari</a
+                >
+              </div>
+            </div>
+          </div>
+          <!-- End Card Kelas -->
+          <!-- Card Kelas -->
+          <div class="col-md-3">
+            <div class="card mb-3">
+              <img
+                src="assets/card5.png"
+                class="card-img-top"
+                alt="Kelas Javascript"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Fullstack Javascript Expert</h5>
+                <a
+                  href="#"
+                  class="btn btn-primary mx-auto"
+                  style="background-color: #ff7300; width: 270px; border: none"
+                  >Pelajari</a
+                >
+              </div>
+            </div>
+          </div>
+          <!-- End Card Kelas -->
+          <!-- Card Kelas -->
+          <div class="col-md-3">
+            <div class="card mb-3">
+              <img
+                src="assets/card6.png"
+                class="card-img-top"
+                alt="Kelas Javascript"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Fullstack Javascript Expert</h5>
+                <a
+                  href="#"
+                  class="btn btn-primary mx-auto"
+                  style="background-color: #ff7300; width: 270px; border: none"
+                  >Pelajari</a
+                >
+              </div>
+            </div>
+          </div>
+          <!-- End Card Kelas -->
+          <!-- Card Kelas -->
+          <div class="col-md-3">
+            <div class="card mb-3">
+              <img
+                src="assets/card1.png"
+                class="card-img-top"
+                alt="Kelas Javascript"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Fullstack Javascript Expert</h5>
+                <a
+                  href="#"
+                  class="btn btn-primary mx-auto"
+                  style="background-color: #ff7300; width: 270px; border: none"
+                  >Pelajari</a
+                >
+              </div>
+            </div>
+          </div>
+          <!-- End Card Kelas -->
+          <!-- Card Kelas -->
+          <div class="col-md-3">
+            <div class="card mb-3">
+              <img
+                src="assets/card2.png"
+                class="card-img-top"
+                alt="Kelas Javascript"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Fullstack Javascript Expert</h5>
+                <a
+                  href="#"
+                  class="btn btn-primary mx-auto"
+                  style="background-color: #ff7300; width: 270px; border: none"
+                  >Pelajari</a
+                >
+              </div>
+            </div>
+          </div>
+          <!-- End Card Kelas -->
+        </div>
+      </div>
+    </div>
+    <!-- End-Kelas -->
+    ?>
